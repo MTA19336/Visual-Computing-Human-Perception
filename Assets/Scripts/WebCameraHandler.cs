6ead7fbcb137c616f7ebc94e1a1f97616d74036b
Marcus <m.h.otterstreom@gmail.com>
2019-10-21 15:35:37 +0200

    Added WebCamHandler and Changed WebCam Script

---
+++ Assets/Scripts/WebCameraHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WebCameraHandler : MonoBehaviour
+{
+    protected internal uint webCamDeviceIndex = 0;
+    protected internal WebCamDevice[] webCamDevices;
+    protected internal WebCamTexture webCamTexture;
+
+    protected virtual void Awake()
+    {
+        webCamDevices = WebCamTexture.devices;
+        webCamTexture = new WebCamTexture();
+    }
+
+    protected virtual void Start()
+    {
+        if (WebCamTexture.devices.Length == 0)
+        {
+            Debug.Log("WebCamDevice not found.");
+            return;
+        }
+        webCamTexture.deviceName = webCamDevices[webCamDeviceIndex].name;
+        webCamTexture.Play();
+    }
+
+    protected virtual void Update()
+    {
+        WebCamInput(webCamTexture);
+    }
+
+    protected abstract void WebCamInput(WebCamTexture input);
+
+    [ContextMenu("Change Web Camera")]
+    private void nextWebCamDevice()
+    {
+        webCamTexture.Stop();
+        if (webCamDeviceIndex >= WebCamTexture.devices.Length - 1)
+        {
+            webCamDeviceIndex = 0;
+            webCamTexture.deviceName = webCamDevices[webCamDeviceIndex].name;
+        }
+        else
+        {
+            webCamDeviceIndex++;
+            webCamTexture.deviceName = webCamDevices[webCamDeviceIndex].name;
+        }
+        webCamTexture.Play();
+    }
+}
