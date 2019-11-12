787581ac251ad48a2abdc204ce74855e0b8b6953
Marty <Zexyqag@gmail.com>
2019-11-12 10:42:59 +0100

    added demo scene for inspiration

--- Assets/Scripts/WebCamera.cs
+++ Assets/Scripts/WebCamera.cs
@@ -1,23 +1,27 @@
 ﻿namespace OpenCvSharp.Demo
 {
-	using OpenCvSharp;
-	using System.Collections.Generic;
-	using System.Linq;
 	using UnityEngine;
 	using UnityEngine.UI;
 
-    [RequireComponent(typeof(RawImage))]
-    public class WebCamera : WebCameraHandler
-    {
-        private RawImage rawImage;
-        protected override void Awake()
-        {
-            base.Awake();
-            rawImage = gameObject.GetComponent<RawImage>();
-        }
-        protected override void WebCamInput(WebCamTexture input)
-        {
-            rawImage.texture = input;
-        }
-    }
+	[RequireComponent(typeof(RawImage))]
+	public class WebCamera : WebCameraHandler
+	{
+		private RawImage rawImage;
+		private RectTransform rectTransform;
+		[SerializeField] private bool UseMinAspect;
+		protected override void Awake()
+		{
+			base.Awake();
+			rectTransform = gameObject.GetComponent<RectTransform>();
+			rawImage = gameObject.GetComponent<RawImage>();
+		}
+		protected override void CamUpdate(WebCamTexture input)
+		{
+			float aspect = UseMinAspect ? Mathf.Min(Screen.width / (float)input.width, Screen.height / (float)input.height) : Mathf.Max(Screen.width / (float)input.width, Screen.height / (float)input.height);
+			rectTransform.sizeDelta = new Vector2(input.width * aspect, input.height * aspect);
+
+			rawImage.texture = input;
+		}
+	}
 }
+
