78dfd8f9b076e974ab9ddc3ca07c2f727007c319
Marcus <m.h.otterstreom@gmail.com>
2019-12-09 13:05:01 +0100

    fpsfix

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -4,14 +4,23 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class FPSCounter : MonoBehaviour {
 	private Text FPSText;
-	private float FPSVel, FPS;
+	private float FPSVel, FPS, averageFPS;
+    private int totalFrames, currFPS;
 	private void Awake() {
 		FPSText = GetComponent<Text>();
 	}
 
+
 	private void Update() {
 		FPS = Mathf.SmoothDamp(FPS, (1.0f / Time.deltaTime), ref FPSVel, 1);
-		FPSText.text = "FPS: " + Mathf.RoundToInt(FPS);
+        currFPS = (int)(1f / Time.unscaledDeltaTime);
+        updateAvrageFPS(currFPS);
+		FPSText.text = "FPS: " + Mathf.RoundToInt(FPS) + "\n Avg. FPS: "+ averageFPS.ToString();
 	}
+
+    private void updateAvrageFPS(float newFPS) {
+        ++totalFrames;
+        averageFPS += (newFPS - averageFPS) / totalFrames;
+    }
 
 }
